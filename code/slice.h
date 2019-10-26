52e58d23fc36410d88b6279031bd7576a168e6fb
l-iberty <2593093242@qq.com>
2019-10-26 19:59:53 +0800

    update

--- code/slice.h
+++ code/slice.h
@@ -6,29 +6,13 @@
 
 class Slice {
 public:
-    Slice()
-        : data_(nullptr)
-        , size_(0)
-    {
-    }
+    Slice() : data_(nullptr), size_(0) {}
 
-    Slice(const char* d, size_t n)
-        : data_(d)
-        , size_(n)
-    {
-    }
+    Slice(const char* d, size_t n) : data_(d) , size_(n) {}
 
-    Slice(const std::string& s)
-        : data_(s.data())
-        , size_(s.size())
-    {
-    }
+    Slice(const std::string& s) : data_(s.data()) , size_(s.size()) {}
 
-    Slice(const char* s)
-        : data_(s)
-        , size_(strlen(s))
-    {
-    }
+    Slice(const char* s) : data_(s) , size_(strlen(s)) {}
 
     Slice(const Slice&) = default;
 
